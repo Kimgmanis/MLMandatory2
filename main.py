4e09c6a82d83444fa39465d3ffe746dc65d626f6
Kim Keiser <Kimalexanderkeiser@gmail.com>
2023-06-13 21:03:40 +0200

    dropped redundant data, removed white space, converted games names to lower case, combined data = games + userDataset .

--- main.py
+++ main.py
@@ -44,13 +44,38 @@ games = games.drop('recent_reviews', axis=1)  # Remove the original 'recent_revi
 # str.split based on ',' and assigning the resulting list to a column called 'tags'
 games['tags'] = games['popular_tags'].str.split(',')
 games = games.drop('popular_tags', axis=1)  # Used drop function to remove the original 'popular_tags'
+games.dropna(inplace=True)  # drops rows with nan
 
 # Split comma-separated values and create new rows
 new_rows = userDataset['games'].apply(lambda x: pd.Series(x))
+
 # Concatenate new rows with the original DataFrame
 userDataset = pd.concat([userDataset.drop('games', axis=1), new_rows], axis=1)
+
 # Select and display specific columns
 selected_columns = ['name', 'playtime_forever']  # playtime_forever is total number of minutes played on a game
 
 print(userDataset[selected_columns])
 print(games)
+
+# Strip leading and trailing whitespaces from game names in both datasets
+userDataset['name'] = userDataset['name'].str.strip()
+games['name'] = games['name'].str.strip()
+
+# Convert game names to lowercase in both datasets
+userDataset['name'] = userDataset['name'].str.lower()
+games['name'] = games['name'].str.lower()
+
+# Merge userDataset with games based on the game name
+combined_data = pd.merge(userDataset, games, on='name', how='right')
+
+# Print the combined data
+print(combined_data['name'])
+print(combined_data['playtime_forever'])
+print(combined_data['all_review_sentiment'])
+print(combined_data['all_review_count'])
+print(combined_data['all_review_percentage'])
+print(combined_data['recent_review_sentiment'])
+print(combined_data['recent_review_count'])
+print(combined_data['recent_review_percentage'])
+print(combined_data.columns)
