ba1ed86d11d738ea9cabbddd15ef8d5e159b44c4
Kim Keiser <Kimalexanderkeiser@gmail.com>
2023-06-13 18:49:19 +0200

    Seperated all/recent reviews.

--- main.py
+++ main.py
@@ -24,12 +24,28 @@ userGames = steam.users.get_owned_games(accountID)
 # Searches for a game
 searchGame = steam.apps.get_app_details(terraria_app_id)
 
-# Data set Ini
+# Data ini
 gamesDataset = pd.read_csv('steam_games.csv')
 userDataset = pd.DataFrame.from_dict(userGames)
 
-# Labels
+# Formatting Labels
 games = gamesDataset.loc[:, ['name', 'recent_reviews', 'all_reviews', 'popular_tags']]
+
+games1 = gamesDataset.loc[:, ['all_reviews']]
+games1[['all_review_sentiment', 'all_review_count', 'all_review_percentage']] =\
+    games1['all_reviews'].str.extract(r'([A-Za-z\s]+),\(([\d,]+)\),-\s([\d%]+)')
+games1 = games1.drop('all_reviews', axis=1)  # Used drop to remove the original 'popular_tags' column
+
+games2 = gamesDataset.loc[:, ['recent_reviews']]
+# Split the 'recent_reviews' column into separate columns
+games2[['recent_review_sentiment', 'recent_review_count', 'recent_review_percentage']] =\
+    games2['recent_reviews'].str.extract(r'([A-Za-z\s]+),\(([\d,]+)\),-\s([\d%]+)')
+games = games.drop('recent_reviews', axis=1)  # Remove the original 'recent_reviews' column
+
+games3 = gamesDataset.loc[:, ['name', 'popular_tags']]  # test3
+# str.split based on ',' and assigning the resulting list to a column called 'tags'
+games3['tags'] = games['popular_tags'].str.split(',')
+games3 = games3.drop('popular_tags', axis=1)  # Used drop function to remove the original 'popular_tags'
 
 # Split comma-separated values and create new rows
 new_rows = userDataset['games'].apply(lambda x: pd.Series(x))
@@ -40,3 +56,26 @@ selected_columns = ['name', 'playtime_forever']  # playtime_forever is total num
 
 print(userDataset[selected_columns])
 print(games)
+print(games1)
+print(games2)
+print(games3)
+
+# Recent reviews means last 30 days
+print("Recent Review Sentiment:")
+print(games2['recent_review_sentiment'])
+
+print("Recent Review Count:")
+print(games2['recent_review_count'])
+
+print("Recent Review Percentage:")
+print(games2['recent_review_percentage'])
+
+# all reviews means since app was created
+print("All Review Sentiment:")
+print(games1['all_review_sentiment'])
+
+print("All Review Count:")
+print(games1['all_review_count'])
+
+print("All Review Percentage:")
+print(games1['all_review_percentage'])
