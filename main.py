c43eeefc6b8a21d1f97550ae6f58ccc6b501f442
Kim Keiser <Kimalexanderkeiser@gmail.com>
2023-06-13 17:06:39 +0200

    Updated notes

--- main.py
+++ main.py
@@ -7,34 +7,36 @@ from pylab import bone, pcolor, colorbar, plot, show
 from steam import Steam
 from decouple import config
 
-KEY = config("STEAM_API_KEY")
-steam = Steam(KEY)
+# API Key Stored in environmental variables
+KEY = config("STEAM_API_KEY")  # Link: https://pypi.org/project/steam/
+steam = Steam(KEY)  # Wrapper for API
 
 # ref
 account = "Kingmanis1929"
 accountID = "76561198052658415"
+# App id: to get specific apps
 terraria_app_id = 105600
 
 # arguments: steamid
-# Retrieves user details
-user = steam.users.search_user(account)
-# Retrieves user game details
+user = steam.users.search_user(account)  # User details / Easy and quick way to obtain user ID
+# User game details
 userGames = steam.users.get_owned_games(accountID)
 # Searches for a game
 searchGame = steam.apps.get_app_details(terraria_app_id)
-# print(user)
-# print(userGames)
-# print(searchGame)
 
+# Data set Ini
 gamesDataset = pd.read_csv('steam_games.csv')
 userDataset = pd.DataFrame.from_dict(userGames)
+
+# Labels
 games = gamesDataset.loc[:, ['name', 'recent_reviews', 'all_reviews', 'popular_tags']]
+
 # Split comma-separated values and create new rows
 new_rows = userDataset['games'].apply(lambda x: pd.Series(x))
 # Concatenate new rows with the original DataFrame
 userDataset = pd.concat([userDataset.drop('games', axis=1), new_rows], axis=1)
-# playtime_forever is total number of minutes played on a game
 # Select and display specific columns
-selected_columns = ['name', 'playtime_forever']
+selected_columns = ['name', 'playtime_forever']  # playtime_forever is total number of minutes played on a game
+
 print(userDataset[selected_columns])
 print(games)
